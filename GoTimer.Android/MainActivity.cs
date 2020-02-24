49fa8b7bec5cdc4fce7a5d8452d6f2e086789a81
Nick <ngable@ldartools.com>
2020-02-23 21:19:42 -0600

    Icon and title works

--- GoTimer.Android/MainActivity.cs
+++ GoTimer.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace GoTimer.Droid
 {
-    [Activity(Label = "GoTimer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Time's Up!", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
